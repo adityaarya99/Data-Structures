c2d1684a34891017560b0711ace97ccedc6d52c2
Aditya <aryaadityacr7@gmail.com>
2019-12-27 04:17:20 +0530

    first

---
+++ Recursion/DS/src/power.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ class Solution {
+
+	public static int power(int x, int n) {
+	if(n==0)
+        {
+        return 1;
+        }
+        if(n==1)
+            {
+            return x;
+            }
+        else
+            {
+        return (x * power(x,n-1));
+          }
+		
+	}
+}
+
+
+public class power {
+		static Scanner s = new Scanner(System.in);
+
+		public static void main(String[] args) {
+			int x = s.nextInt();
+			int n = s.nextInt();
+			
+			System.out.println(Solution.power(x, n));
+	}
+
+}
