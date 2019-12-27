b47bf0bf6d4b087a10515fc1141cecf6775ff0a2
Aditya <aryaadityacr7@gmail.com>
2019-12-28 05:05:23 +0530

    Multi

---
+++ Recursion - Practise Ques/DS/src/Multiplication.java
@@ -0,0 +1,27 @@
+/*Given two integers m & n, calculate and return their 
+multiplication using recursion. You can only use subtraction 
+and addition for your calculation. No other operators are allowed.*/
+import java.util.*;
+public class Multiplication {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		System.out.println(Multi.multiplyTwoIntegers(m, n));
+	}
+}
+
+ class Multi {
+
+	public static int multiplyTwoIntegers(int m, int n){
+		if (m < n) 
+            return multiplyTwoIntegers(n, m);
+        else if (n != 0)
+        { 
+      return (m + multiplyTwoIntegers(m, n - 1));
+        }
+        else
+            return 0;
+        }
+}
+
