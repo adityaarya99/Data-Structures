ac23e4b5311db5886425222bfc793d3afb128fe9
Aditya <aryaadityacr7@gmail.com>
2020-01-12 23:24:49 +0530

    BS

--- Linked List 2/LL2/Delete/DN.java
+++ Linked List 2/LL2/Delete/DN.java
@@ -1,4 +1,18 @@
-
 public class DN {
 
+	public static LinkedListNode<Integer> deleteIthNodeRec(LinkedListNode<Integer> head, int i){
+		
+		if (i > 0 && head.next == null) {
+        return head;
+    }
+
+    if (i == 0 || head.next == null) {
+        return head.next;
+    }
+
+    head.next = deleteIthNodeRec(head.next, i - 1);
+    return head;        
+
+		
+	}
 }
